80122ebb6b1731f55ac2de5d87687b17bf9f2d39
AbdulMoeed <67968476+abdulmoeedm2020@users.noreply.github.com>
2025-09-26 09:29:03 +0500

    Update 1.Change MAC Address.py

--- 1.Change MAC Address.py
+++ 1.Change MAC Address.py
@@ -1,3 +1,5 @@
 import subprocess
-subprocess.call("ifconfig", Shell=True)
-  
+
+subprocess.call("ifconfig wlan0 down", Shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", Shell=True)
+subprocess.call("ifconfig wlan0 up", Shell=True)
